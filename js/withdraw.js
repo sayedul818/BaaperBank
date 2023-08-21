b1892d53b10ae1733722cc2c34a24d2a2bd0ea54
sayedul-islam-sayed <sayedulsayed818@gmail.com>
2023-08-21 23:58:13 +0600

    6th comment

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,21 +1,25 @@
 // step-01: Add event listener to the withdraw button
 document.getElementById("withdraw-btn").addEventListener("click",function(){
-// step-02:  get the withdraw ammount from the withdraw input field
-// For input field use .value
-    const withdrawInputField=document.getElementById("withdraw-input-field");
-    const newWithdrawRequest=withdrawInputField.value;
-// step-03:
-    const withdrawTotalBalance=document.getElementById("withdraw-total-amount");
-    const previousTotalWithdrawBalance=withdrawTotalBalance.innerText;
-// step-04:
-    const currentWithdrawTotalBalance=parseFloat(newWithdrawRequest)+parseFloat(previousTotalWithdrawBalance);
-    withdrawTotalBalance.innerText=currentWithdrawTotalBalance;
-// step-05:
-    const bankBalanceTotal=document.getElementById("bank-balance-total");
-    const previousBankBalanceTotal=bankBalanceTotal.innerText;
-// step-06:
-    const currentBankBalanceTotal=parseFloat(previousBankBalanceTotal)-parseFloat(newWithdrawRequest);
-    bankBalanceTotal.innerText=currentBankBalanceTotal;
-// step-07: clear the withdraw input field
-    withdrawInputField.value="";
-})
+    // step-02:  get the withdraw ammount from the withdraw input field
+    // For input field use .value
+        const withdrawInputField=document.getElementById("withdraw-input-field");
+        const newWithdrawRequest=withdrawInputField.value;
+    // step-03: clear the withdraw input field
+        withdrawInputField.value="";
+    // step-04:
+        const withdrawTotalBalance=document.getElementById("withdraw-total-amount");
+        const previousTotalWithdrawBalance=withdrawTotalBalance.innerText;
+    // step-06:
+        const bankBalanceTotal=document.getElementById("bank-balance-total");
+        const previousBankBalanceTotal=bankBalanceTotal.innerText;
+        if(newWithdrawRequest>previousBankBalanceTotal){
+            alert("insufficient Balance");
+            return;
+        }
+    // step-05:
+        const currentWithdrawTotalBalance=parseFloat(newWithdrawRequest)+parseFloat(previousTotalWithdrawBalance);
+        withdrawTotalBalance.innerText=currentWithdrawTotalBalance;
+    // step-07:
+        const currentBankBalanceTotal=parseFloat(previousBankBalanceTotal)-parseFloat(newWithdrawRequest);
+        bankBalanceTotal.innerText=currentBankBalanceTotal;
+    })
